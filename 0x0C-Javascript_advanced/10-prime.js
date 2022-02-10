eaf24b90ef6f9615bbd58ec00ddbc7a9af766595
M33x0 <michael.mbea@gmail.com>
2022-02-10 14:12:22 +0100

    prime

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,22 @@
+function countPrimeNumbers () {
+  const sieve = [];
+  let total = 0;
+
+  for (let i = 2; i <= 100; ++i) {
+    if (!sieve[i]) {
+      total += 1;
+      for (let j = i << 1; j <= 100; j += i) {
+        sieve[j] = true;
+      }
+    }
+  }
+  return total;
+}
+
+const startTime = window.performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+const endTime = window.performance.now();
+
+console.log('Execution time of calculating prime numbers 100 times was ' + (endTime - startTime) + ' milliseconds.');
